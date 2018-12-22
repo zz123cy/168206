a4524e5f1da88ed477a71f750c616056753b7a82
zz123cy <44491751+zz123cy@users.noreply.github.com>
2018-12-22 14:23:09 +0800

    Delete 考试.py

--- 168206143/考试.py
+++
@@ -1,8 +0,0 @@
-print("A：我没有偷钻石。")
-print("B：D就是罪犯。")
-print("C：B是盗窃这块钻石的罪犯。")
-print("D：B有意诬陷我。")
-
-for Crime in ['A','B','C','D']:
-    if(1==(Crime!='A')+(Crime=='D')+(Crime=='B')+(Crime!='D')):
-        print("罪犯是：",Crime)
